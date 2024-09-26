2476893bdd969ffc4d8755aba8726eb62f917e21
guilhermers21 <guilhermetavaresrs@gmail.com>
2024-09-26 13:59:07 -0300

    Add files via upload

---
+++ cadastro_imoveis.sql
@@ -0,0 +1,116 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 26, 2024 at 06:56 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cadastro_imoveis`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `imoveis`
+--
+
+CREATE TABLE `imoveis` (
+  `id` int(11) NOT NULL,
+  `logradouro` varchar(255) NOT NULL,
+  `numero` int(11) NOT NULL,
+  `bairro` varchar(255) NOT NULL,
+  `complemento` varchar(255) DEFAULT NULL,
+  `contribuinte_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `imoveis`
+--
+
+INSERT INTO `imoveis` (`id`, `logradouro`, `numero`, `bairro`, `complemento`, `contribuinte_id`) VALUES
+(3, 'sao roque', 340, 'cristo rei', 'julio de castilhos', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pessoas`
+--
+
+CREATE TABLE `pessoas` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `data_nascimento` date NOT NULL,
+  `cpf` varchar(14) NOT NULL,
+  `sexo` char(1) NOT NULL,
+  `telefone` varchar(20) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pessoas`
+--
+
+INSERT INTO `pessoas` (`id`, `nome`, `data_nascimento`, `cpf`, `sexo`, `telefone`, `email`) VALUES
+(2, 'Iuri martins', '1978-02-05', '45637808790', 'M', '519897087456', 'iuri2022@gmail.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `imoveis`
+--
+ALTER TABLE `imoveis`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `contribuinte_id` (`contribuinte_id`);
+
+--
+-- Indexes for table `pessoas`
+--
+ALTER TABLE `pessoas`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `cpf` (`cpf`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `imoveis`
+--
+ALTER TABLE `imoveis`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `pessoas`
+--
+ALTER TABLE `pessoas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `imoveis`
+--
+ALTER TABLE `imoveis`
+  ADD CONSTRAINT `imoveis_ibfk_1` FOREIGN KEY (`contribuinte_id`) REFERENCES `pessoas` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
