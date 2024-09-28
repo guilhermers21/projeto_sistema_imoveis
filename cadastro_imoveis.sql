95d38bbb8695096bd53f91ad9861b1bf09129597
guilhermers21 <guilhermetavaresrs@gmail.com>
2024-09-28 14:28:05 -0300

    Add files via upload

--- cadastro_imoveis.sql
+++ cadastro_imoveis.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 26, 2024 at 06:56 PM
+-- Generation Time: Sep 28, 2024 at 07:27 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -66,6 +66,25 @@ CREATE TABLE `pessoas` (
 INSERT INTO `pessoas` (`id`, `nome`, `data_nascimento`, `cpf`, `sexo`, `telefone`, `email`) VALUES
 (2, 'Iuri martins', '1978-02-05', '45637808790', 'M', '519897087456', 'iuri2022@gmail.com');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `username`, `password`) VALUES
+(1, 'admin', '$2y$10$dB7gAtCie9fjlYxsve3pV.5/UzoXMI6Xj2Byk61amX2FH6sAdba5W');
+
 --
 -- Indexes for dumped tables
 --
@@ -85,6 +104,13 @@ ALTER TABLE `pessoas`
   ADD UNIQUE KEY `cpf` (`cpf`);
 
 --
+-- Indexes for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -99,6 +125,12 @@ ALTER TABLE `imoveis`
 --
 ALTER TABLE `pessoas`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
